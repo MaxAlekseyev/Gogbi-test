511cd7c948ee8ece66339e780102871df5340e5a
Max Alekseyev <maxalekseyev@gmail.com>
2023-12-02 19:47:53 +0200

    For testing v.1

--- js/base.js
+++ js/base.js
@@ -1,21 +1,37 @@
-const breakPoint = 768;
+const breakPoint = 960;
 const mediaQuery = `(max-width: ${breakPoint}px)`;
 const mediaQueryList = window.matchMedia(mediaQuery);
 const body = document.body;
+const menu = document.querySelector('[data-role="menu"]');
+const overlay = document.querySelector('#overlay');
+const closeMenu = document.querySelector('#toogle_menu');
 
 const desktop = () => {
     body.classList.add('desktop')
     body.classList.remove('mobile')
-
+    body.classList.remove('menu-on')
 }
+
 const mobile = () => {
     body.classList.add('mobile')
     body.classList.remove('desktop')
 }
 
-const query = e =>  e.matches ? mobile() : desktop() ;
+const query = e => e.matches ? mobile() : desktop();
 window.innerWidth > breakPoint ? desktop() : mobile();
 
 mediaQueryList.addEventListener('change', e => query(e))
+
+const statusButtons = document.querySelectorAll('.status-button');
+
+statusButtons.forEach(item => item.addEventListener('click', e => {
+    e.target.classList.toggle('inactive')
+}));
+
+const toogleMenu = () => body.classList.toggle('menu-on');
+
+menu.addEventListener('click', toogleMenu);
+overlay.addEventListener('click', toogleMenu);
+closeMenu.addEventListener('click', toogleMenu);
 
 
